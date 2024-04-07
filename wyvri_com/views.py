0a1a4a18afcb6b5c3b16fd4f0ac0e48092ad3de7
wyvri <wrennuin@gmail.com>
2024-04-07 13:10:32 -0500

    get stuff working

---
+++ wyvri_com/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base(request):
+    context={
+
+    }
+
+    return render(request, 'base.html', context)
+
+def home(request):
+    context={
+
+    }
+
+    return render(request, 'home.html', context)
