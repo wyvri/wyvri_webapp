87fab3cc3dce3e98ae02bc842220c2e55b1ecc7b
wyvri <wrennuin@gmail.com>
2024-05-05 14:39:40 -0500

    make the images random sizes

--- wyvri_com/views.py
+++ wyvri_com/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Art
+import random
 
 # Create your views here.
 
@@ -24,9 +25,17 @@ def aboutme(request):
 
     return render(request, 'aboutme.html', context)
 
+def get_size():
+    sizeList = [150, 200, 200, 250, 300]
+    return random.choice(sizeList)
+
+def add_size(art):
+    art.size = get_size()
+    return art
+
 def gallery(request):
     context={
-        'arts':Art.objects.all()
+        'arts':map(add_size, Art.objects.all())
     }
 
     return render(request, 'gallery.html', context)
